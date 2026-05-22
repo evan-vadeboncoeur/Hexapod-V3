4ef4eb8f9eb958422fb430769836ccbad173810a
Evan Vadeboncoeur <e_vadeboncoeur@WKH.WKHillquist.com>
2026-05-22 08:14:43 -0400

    commit_msg

---
+++ push.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Stage all files changes
+git add .
+
+# Prompt user for input
+echo "Please enter commit message."
+
+# Get user input commit message
+read commit_msg
+
+# Commit changes
+git commit -m commit_msg
+
+# Push to origin of repository
+git push origin head
