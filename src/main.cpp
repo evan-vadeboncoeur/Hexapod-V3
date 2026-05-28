0327bd63d6b4293a78470f2e90bdc903928dc0b3
evanjvad <ejvadeboncoeur2@wpi.edu>
2026-05-27 20:51:21 -0400

    Servo test

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,17 @@
 #include <Arduino.h>
 #include "Hexapod.h"
 
-
 // might have to do joints & everything else BEFORE runtime, too, so that it's on the heap, not the stack?
+Servo s = Servo();
 
-Leg L0 = Leg(1, 2, 3, 0);
-Leg L1 = Leg(4, 5, 6, 1);
-Leg L2 = Leg(7, 8, 9, 2);
-Leg L3 = Leg(10, 11, 12, 3);
-Leg L4 = Leg(13, 14, 15, 4);
-Leg L5 = Leg(16, 17, 18, 5);
+//Leg L0 = Leg(1, 2, 3, 0);
+// Leg L1 = Leg(4, 5, 6, 1);
+// Leg L2 = Leg(7, 8, 9, 2);
+// Leg L3 = Leg(10, 11, 12, 3);
+// Leg L4 = Leg(13, 14, 15, 4);
+// Leg L5 = Leg(16, 17, 18, 5);
 
-Leg Legs[6] = {L0, L1, L2, L3, L4, L5};
+//Leg Legs[6] = {L0, L1, L2, L3, L4, L5};
 #ifndef IK_DEBUG
 MotionPlanner plan = MotionPlanner(Legs);
 
@@ -24,12 +24,13 @@ C_Position test_2 = C_Position(-150.0, -221.0, 67.0);
 
 void setup(){
   Serial.begin(9600);
+  s.attach(9);
 }
 
 void loop() {
   delay(2000);
-  
-  L0.moveToIK(test_2, true);
+  s.write(map(135, FOOT_SERVO_MIN, FOOT_SERVO_MAX_INT, PWM_MIN, PWM_MAX));
+  //L0.moveToIK(test_2, true);
   #ifdef IK_DEBUG
   delay(2000);
   exit(1);
