e2c74ba5a64880f128893681695e3fd053eaeb56
evanjvad <ejvadeboncoeur2@wpi.edu>
2026-06-28 20:35:17 -0400

    PCB v1 tested

--- Robot Board/HPD Robot Board/src/main.cpp
+++ Robot Board/HPD Robot Board/src/main.cpp
@@ -2,14 +2,14 @@
 #include "Body.h"
 
 //#include "Hexapod.h"
-//#define LEG_SETUP_DEBUG
+#define LEG_SETUP_DEBUG
 
 // might have to do joints & everything else BEFORE runtime, too, so that it's on the heap, not the stack?
 #ifdef LEG_SETUP_DEBUG
 Servo s0, s1, s2;
-int j0 = LEG_0_J0;
-int j1 = LEG_0_J1;
-int j2 = LEG_0_J2;
+int j0 = LEG_1_J0;
+int j1 = LEG_1_J1;
+int j2 = LEG_1_J2;
 #endif
 
 float duty_f = 0.5;
